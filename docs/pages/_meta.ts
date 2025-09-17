89afcc4ad0908824828381f80be1e4f75ff5e659
Ewall555 <a03216@foxmail.com>
2025-09-17 06:09:37 +0000

    perf: 添加联系我们

--- docs/pages/_meta.ts
+++ docs/pages/_meta.ts
@@ -6,5 +6,6 @@ export default {
   v3: { title: '5. JumpServer V3 版本介绍' },
   v4: { title: '6. JumpServer V4 版本介绍' },
   pam: { title: '7. JumpServer PAM 特权账号管理' },
-  cases: { title: '8. 案例研究&社区分享' }
+  cases: { title: '8. 案例研究&社区分享' },
+  contact: {title: '9. 联系我们' }
 }
